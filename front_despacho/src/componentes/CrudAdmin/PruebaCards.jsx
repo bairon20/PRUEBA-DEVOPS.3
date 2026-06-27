c9851473eea0ceb835570f8cb3ea0cd02628413d
bairon20 <br.gomezv@duocuc.cl>
2026-06-26 21:46:56 -0400

    Update PruebaCards.jsx

--- front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
+++ front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
@@ -11,7 +11,7 @@ export const PruebaCards = () => {
     <section>
       <div className="flex justify-center">
         <CardComponent
-          title="Crear Orden despacho2026"
+          title="Crear Orden despacho"
           description="Revisa las ordenes de compra disponibles y genera un despacho nuevo"
           buttonText="Crear despacho"
           onClick={() => {
